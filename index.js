10ea315bab1e07012241a131115e1548b648a719
Ugo <ugo.arzur@morning.com>
2017-06-12 10:52:40 +0200

    better error handler

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const bot = new Discord.Client()
 // Login bot
 bot.login('MjgzMTY3NTIzNzgyNzg3MDcy.DB8Xig.BasDJKFfWAmIS8WLuinCgaHs5tw')
 
-// Définit l'avatar
+// Set avatar when ready
 bot.on('ready', () => {
     bot.user.setAvatar('./img/harrybotteur.jpg')
+        .then(() => console.log('Avatar mis avec succès'))
+        .catch(console.error)
 })
 
 // When receive a message
