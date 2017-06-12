e942ea9ea843f6c3dc0635968023d7fe1f411fb9
Ugo <ugo.arzur@morning.com>
2017-06-12 10:26:54 +0200

    ajout wording

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
-const config = require('./config.json')
+
+// Login bot
+bot.login('MjgzMTY3NTIzNzgyNzg3MDcy.DB8Xig.BasDJKFfWAmIS8WLuinCgaHs5tw')
 
 // Définit l'avatar
 bot.on('ready', () => {
     bot.user.setAvatar('./img/harrybotteur.jpg')
 })
 
-bot.login(config.token)
-// bot.on('message', (message) => {
-//     if (message.content === 'ping'){
-//         message.reply('pong')
-//     }
-// })
+// When receive a message
 bot.on('message', (message) => {
     if (message.content === 'ping'){
-        message.channel.send('pong')
+        message.reply('pong')
+    } else if(message.content === 'infos'){
+        message.reply('Sur quoi ?')
     }
 })
